d44b0395bb334626bffa0c2aad283cb6bb869bd2
brennareach <brenna.reach@gmail.com>
2026-05-02 18:26:15 -1000

    Create TrainingPlan.py
    Initial commit - basic plan generator

---
+++ TrainingPlan.py
@@ -0,0 +1,11 @@
+##Basic Code
+
+weeks = 4
+
+for week in range(1, weeks + 1):
+    print(f"Week {week}:")
+    print("- Easy run")
+    print("- Tempo run")
+    print("- Long run")
+    print("- Rest day")
+    print()
